d8dff1ee52105aa33cdcda5ff069f93988c56783
mdev1987 <m.developer66@gmail.com>
2025-05-05 08:54:15 +0330

    Add test cases for flashloan execution and utility functions

---
+++ test/index.test.ts
@@ -0,0 +1,50 @@
+import { ethers } from "ethers";
+import { deployDodoFlashloan } from "../scripts/deployDodoFlashloan";
+import { dodoV2Pool, Protocols } from "../constants";
+import { findRouterByProtocol } from "../utils/findRouterByProtocol";
+import { FlashLoanParams } from "../types";
+import { ERC20Token } from "../constants/tokens";
+import { executeFlashloan } from "../scripts/executeFlashloan";
+
+describe("DODO FlashLoan", () => {
+  it("Execute flash loan", async () => {
+    //const providerUrl = "http://localhost:8545/";
+    const privateKey = process.env.PRIVATE_KEY || "";
+    const wallet = new ethers.Wallet(privateKey);
+    const flashLoan = await deployDodoFlashloan({ wallet });
+    const params: FlashLoanParams = {
+      flashLoanContractAddress: flashLoan.target.toString(),
+      flashLoanPool: dodoV2Pool.WETH_ULT,
+      loanAmount: ethers.parseEther("0.1"),
+      // loanAmountDecimals: 18,
+      hops: [
+        {
+          protocol: Protocols.UNISWAP_V2,
+          data: ethers.AbiCoder.defaultAbiCoder().encode(
+            ["address"],
+            [findRouterByProtocol(Protocols.UNISWAP_V2)]
+          ),
+          path: [ERC20Token.WETH?.address, ERC20Token.USDC?.address],
+          amountOutMinV3: 0,
+          sqrtPriceLimitX96: 0,
+        },
+        {
+          protocol: Protocols.SUSHISWAP,
+          data: ethers.AbiCoder.defaultAbiCoder().encode(
+            ["address"],
+            [findRouterByProtocol(Protocols.SUSHISWAP)]
+          ),
+          path: [ERC20Token.USDC?.address, ERC20Token.WETH?.address],
+          amountOutMinV3: 0,
+          sqrtPriceLimitX96: 0,
+        },
+      ],
+      gasLimit: 3_000_000,
+      gasPrice: ethers.parseUnits("300", "gwei"),
+      signer: wallet,
+    };
+
+    const tx = await executeFlashloan(params);
+    await tx.wait();
+  });
+});
